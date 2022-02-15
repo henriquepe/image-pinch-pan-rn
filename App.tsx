84b1f24f367cd4411027b224ce80b0a041d6cece
hpiress <henrique.p.espindola@gmail.com>
2022-02-15 10:05:07 -0300

    example of pinch and pan rngh

--- App.tsx
+++ App.tsx
@@ -1,19 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { Animated, StyleSheet } from 'react-native';
+import sofa_mobly from './src/assets/sofa_mobly.jpeg';
+import { GestureHandlerRootView, PinchGestureHandler, State, PanGestureHandler } from 'react-native-gesture-handler';
+import React, { useRef, createRef } from 'react';
 
 export default function App() {
+  const scale = useRef(new Animated.Value(1)).current;
+  let translateX = useRef(new Animated.Value(0)).current;
+  let translateY = useRef(new Animated.Value(0)).current;
+  const pinchRef = createRef()
+  const panRef = createRef()
+
+  const onPinchEvent = Animated.event([{
+    nativeEvent: {scale}
+  }], {useNativeDriver: true})
+
+  const onPanEvent = Animated.event([{
+    nativeEvent: {
+      translationX: translateX,
+      translationY: translateY
+    }
+  }], {useNativeDriver: true})
+
+  const handlePinchStateChange = ({nativeEvent}: {nativeEvent: any}) => {
+    const nScale = nativeEvent.scale;
+    if(nativeEvent.state === State.END){
+      if(nScale < 1){
+        Animated.spring(scale, {
+          toValue: 1,
+          useNativeDriver: true 
+        }).start()
+        Animated.spring(translateX, {
+          toValue: 0,
+          useNativeDriver: true
+        }).start();
+        Animated.spring(translateY, {
+          toValue: 0,
+          useNativeDriver: true
+        }).start();
+      }
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={styles.container} >
+      <PanGestureHandler 
+        onGestureEvent={onPanEvent} 
+        ref={panRef} 
+        simultaneousHandlers={[pinchRef]} 
+        shouldCancelWhenOutside
+        failOffsetX={[-1000, 1000]}
+      >
+        <Animated.View>
+          <PinchGestureHandler 
+            onGestureEvent={onPinchEvent} 
+            onHandlerStateChange={handlePinchStateChange}
+            ref={pinchRef}
+            simultaneousHandlers={[panRef]}
+
+          >
+              <Animated.Image 
+                source={sofa_mobly} 
+                style={[{transform: [{ scale }, {translateX}, {translateY}]}]}
+                resizeMode="center"
+              />
+          </PinchGestureHandler>
+        </Animated.View>
+      </PanGestureHandler>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
